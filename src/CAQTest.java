e8c659c31c78f2526e68b15b3d8589ffbdd04bc8
David Brown <db7g12@soton.ac.uk>
2015-11-27 23:54:15 +0000

    Initial commit

---
+++ src/CAQTest.java
@@ -0,0 +1,535 @@
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.NoSuchElementException;
+import java.util.Queue;
+import java.util.Random;
+import java.util.UUID;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class CAQTest
+{
+	CircularArrayQueue<Integer> queue;
+	ArrayList<Integer> test;
+
+	@Before
+	public void setup()
+	{
+		queue = new CircularArrayQueue<Integer>();
+		test = new ArrayList<Integer>();
+	}
+
+	@Test
+	public void ensureCapacity()
+	{
+		queue = new CircularArrayQueue<Integer>(40);
+		assertEquals(40, queue.capacity());
+	}
+
+	@Test
+	public void add()
+	{
+		queue.add(1);
+		assertEquals(1, queue.size());
+		assertTrue(queue.contains(1));
+	}
+
+	@Test
+	public void empty()
+	{
+		assertTrue(queue.isEmpty());
+		queue.add(10);
+		assertEquals(1, queue.size());
+		assertTrue(!queue.isEmpty());
+	}
+
+	@Test
+	public void testRemove()
+	{
+		queue.add(10);
+		queue.add(20);
+		int elem = queue.remove();
+		assertEquals(elem, 10);
+		assertEquals(1, queue.size());
+	}
+
+	@Test
+	public void testAddMultiple()
+	{
+		for (int i = 0; i < 100; i++)
+		{
+			queue.add(i);
+		}
+
+		assertEquals(100, queue.size());
+		for (int i = 0; i < test.size(); i++)
+		{
+			assertTrue(queue.contains(i));
+		}
+	}
+
+	@Test
+	public void testCapacity()
+	{
+		for (int i = 0; i < 10; i++)
+		{
+			queue.add(i);
+			assertEquals(10, queue.capacity());
+		}
+
+		queue.add(10);
+		assertEquals(20, queue.capacity());
+	}
+
+	@Test
+	public void testMultipleAddRemove()
+	{
+		Random rand = new Random();
+		int size = 0;
+		for (int i = 0; i < 10000; i++)
+		{
+			float next = rand.nextFloat();
+			if (size == 0)
+			{
+				next = 0.7f;
+			}
+			if (next >= 0.5)
+			{
+				queue.add(i);
+				test.add(i);
+				size++;
+			} else
+			{
+				queue.remove();
+				test.remove(0);
+				size--;
+			}
+		}
+
+		assertEquals(size, queue.size());
+		assertEquals(test.size(), queue.size());
+		for (Integer i : test)
+		{
+			assertTrue(queue.contains(i));
+		}
+	}
+
+	// TODO Readd
+	// @Test
+	public void testLoadsOfAdding()
+	{
+		Random rand = new Random();
+		int size = 0;
+		for (int i = 0; i < 10000000; i++)
+		{
+			int next = rand.nextInt(1000);
+			queue.add(next);
+			test.add(next);
+			size++;
+			assertEquals(size, queue.size());
+		}
+
+		assertTrue(test.containsAll(queue));
+		assertTrue(queue.containsAll(test));
+	}
+
+	@Test
+	public void testTypeChecking()
+	{
+		queue.add(null);
+		assertTrue(queue.contains(null));
+	}
+
+	@Test(expected = NoSuchElementException.class)
+	public void testEmptyRemove()
+	{
+		queue.remove();
+	}
+
+	@Test(expected = NoSuchElementException.class)
+	public void testElement()
+	{
+		queue.element();
+	}
+
+	@Test
+	public void testPeek()
+	{
+		assertEquals(null, queue.peek());
+	}
+
+	@Test
+	public void algorithmicsTestSimple()
+	{
+		checkSize(0, queue);
+		queue.add(3);
+		checkSize(1, queue);
+		try
+		{
+			assertEquals("Dequeue returns wrong element", 3, (int) queue.remove());
+		} catch (NoSuchElementException e)
+		{
+			throw e;
+		}
+		checkSize(0, queue);
+	}
+
+	private void checkSize(int length, Queue<?> queue)
+	{
+		assertEquals("Queue has wrong number of elements", length, queue.size());
+		if (length == 0)
+		{
+			assertTrue("Queue should be empty", queue.isEmpty());
+		} else
+		{
+			assertTrue("Queue should not be empty", !queue.isEmpty());
+		}
+	}
+
+	@Test
+	public void algorithmicsTestMultiInput()
+	{
+		for (int i = 0; i < 1000; ++i)
+		{
+			int r = (int) Math.round(Math.random());
+			checkSize(0, queue);
+			queue.add(r);
+			checkSize(1, queue);
+			assertEquals("Dequeue returns wrong element", r, (int) queue.remove());
+		}
+	}
+
+	@Test
+	public void algorithmicsTestManyEnqueueDequeue()
+	{
+		int cnt = 0;
+		for (int i = 0; i < 100000; ++i)
+		{
+			if (Math.random() > 0.5)
+			{
+				queue.add(i);
+				cnt++;
+			} else
+			{
+				if (!queue.isEmpty())
+				{
+					queue.remove();
+					cnt--;
+				}
+			}
+			assertEquals("Correct number of items", cnt, queue.size());
+		}
+	}
+
+	@Test
+	public void algorithmicsTestLargeQueue()
+	{
+		int[] r = new int[1000];
+		for (int i = 0; i < r.length; ++i)
+		{
+			r[i] = (int) Math.round(Math.random());
+			checkSize(i, queue);
+			queue.add(r[i]);
+		}
+		for (int i = 0; i < r.length; ++i)
+		{
+			assertEquals("Dequeue returns wrong element", r[i], (int) queue.remove());
+			checkSize(r.length - i - 1, queue);
+		}
+		for (int i = 0; i < r.length; ++i)
+		{
+			r[i] = (int) Math.round(Math.random());
+			checkSize(i, queue);
+			queue.add(r[i]);
+		}
+		for (int i = 0; i < r.length; ++i)
+		{
+			assertEquals("Dequeue returns wrong element", r[i], (int) queue.remove());
+			checkSize(r.length - i - 1, queue);
+		}
+	}
+
+	@Test
+	public void algorithmicsTestThrows()
+	{
+		int[] r = new int[1000];
+		for (int i = 0; i < r.length; ++i)
+		{
+			r[i] = (int) Math.round(Math.random());
+			checkSize(i, queue);
+			queue.add(r[i]);
+		}
+		for (int i = 0; i < r.length; ++i)
+		{
+			assertEquals("Dequeue returns wrong element", r[i], (int) queue.remove());
+			checkSize(r.length - i - 1, queue);
+		}
+		boolean throwsCorrectly = false;
+		try
+		{
+			queue.remove();
+		} catch (NoSuchElementException e)
+		{
+			throwsCorrectly = true;
+		}
+		assertTrue("Throws when dequeuing empty queue", throwsCorrectly);
+	}
+
+	@Test
+	public void algorithmicsTestResize()
+	{
+		assertTrue("Initial capacity too large", queue.capacity() - queue.size() <= 1024);
+		for (int i = 0; i < 1000; ++i)
+		{
+			queue.add(i);
+		}
+		int currentCapacity = queue.capacity() - queue.size();
+		while (currentCapacity > 0)
+		{
+			queue.add(9);
+			currentCapacity--;
+			assertEquals("Array size should not change", currentCapacity, queue.capacity() - queue.size());
+		}
+		assertTrue("Should have reached capacity", queue.capacity() - queue.size() == 0);
+		queue.add(42);
+		assertTrue("Should have resized array", currentCapacity < queue.capacity() - queue.size());
+		currentCapacity = queue.capacity() - queue.size();
+		for (int i = 0; i < 100; ++i)
+		{
+			queue.add(i);
+			currentCapacity--;
+			assertEquals("Resizing too often (inefficient)", currentCapacity, queue.capacity() - queue.size());
+		}
+	}
+
+	@Test
+	public void testIterator()
+	{
+		CircularArrayQueue<String> queue = new CircularArrayQueue<>();
+		ArrayList<String> test = new ArrayList<>();
+
+		Random rand = new Random();
+
+		int size = 0;
+		for (int i = 0; i < 1000; i++)
+		{
+			float next = rand.nextFloat();
+			if (size == 0)
+			{
+				next = 0.9f;
+			}
+
+			if (next >= 0.8f)
+			{
+				String nextString = UUID.randomUUID().toString();
+				test.add(nextString);
+				queue.add(nextString);
+			} else
+			{
+				test.remove(0);
+				queue.remove();
+			}
+		}
+
+		Iterator<String> qi = queue.iterator();
+		Iterator<String> ti = test.iterator();
+
+		String elem = null;
+		while (ti.hasNext())
+		{
+			elem = ti.next();
+			assertEquals(elem, qi.next());
+		}
+
+		assertEquals(ti.hasNext(), qi.hasNext());
+	}
+
+	@Test
+	public void checkToArray()
+	{
+		addRandomInts();
+
+		Integer arr1[] = Arrays.copyOf(test.toArray(), test.size(), Integer[].class);
+		Integer arr2[] = Arrays.copyOf(queue.toArray(), queue.size(), Integer[].class);
+
+		HashSet<Integer> set1 = new HashSet<>(Arrays.asList(arr1));
+		HashSet<Integer> set2 = new HashSet<>(Arrays.asList(arr2));
+		assertTrue(set1.equals(set2));
+	}
+
+	private void addRandomInts()
+	{
+		Random rand = new Random();
+
+		int size = 0;
+		for (int i = 0; i < 1000; i++)
+		{
+			float next = rand.nextFloat();
+			if (size == 0)
+			{
+				next = 0.9f;
+			}
+
+			if (next >= 0.8f)
+			{
+				int nextInt = rand.nextInt(1000);
+				test.add(nextInt);
+				queue.add(nextInt);
+			} else
+			{
+				test.remove(0);
+				queue.remove();
+			}
+		}
+	}
+
+	@Test
+	public void checkToArrayParameterized()
+	{
+		addRandomInts();
+
+		Object[] arr1 = test.toArray(new Object[10]);
+		Object[] arr2 = queue.toArray(new Object[10]);
+
+		HashSet<Object> set1 = new HashSet<Object>(Arrays.asList(arr1));
+		HashSet<Object> set2 = new HashSet<Object>(Arrays.asList(arr2));
+
+		assertTrue(set1.equals(set2));
+	}
+
+	@Test
+	public void testAddAll()
+	{
+		ArrayList<Integer> toAdd = new ArrayList<>();
+
+		for (int i = 0; i < 100; i++)
+		{
+			toAdd.add(i);
+		}
+
+		queue.addAll(toAdd);
+
+		assertEquals(100, queue.size());
+
+		assertTrue(queue.containsAll(toAdd));
+		assertTrue(toAdd.containsAll(queue));
+	}
+
+	@Test
+	public void testRemoveAll()
+	{
+		ArrayList<Integer> toRemove = new ArrayList<>();
+		Random rand = new Random();
+
+		for (int i = 0; i < 100; i++)
+		{
+			toRemove.add(rand.nextInt(100));
+			test.add(i);
+			queue.add(i);
+		}
+
+		queue.removeAll(toRemove);
+		test.removeAll(toRemove);
+
+		assertTrue(test.containsAll(queue));
+		assertTrue(queue.containsAll(test));
+		assertEquals(test.size(), queue.size());
+	}
+
+	@Test
+	public void testMultipleRemoveAndSize()
+	{
+		Queue<Integer> test = new LinkedList<>();
+		Random rand = new Random();
+
+		int size = 0;
+		for (int i = 0; i < 100000; i++)
+		{
+			float next = rand.nextFloat();
+			if (size == 0)
+			{
+				next = 0.9f;
+			}
+
+			if (next >= 0.8f)
+			{
+				int nextInt = rand.nextInt(1000000);
+				test.add(nextInt);
+				queue.add(nextInt);
+			} else
+			{
+				test.remove(0);
+				queue.remove();
+			}
+		}
+
+		assertEquals(test.size(), queue.size());
+	}
+
+	@Test
+	public void testRemoveObjects()
+	{
+		ArrayList<String> test = new ArrayList<String>(150);
+		Queue<String> queue = new CircularArrayQueue<String>(150);
+
+		Random rand = new Random();
+
+		int size = 0;
+		for (int i = 0; i < 75; i++)
+		{
+			float next = rand.nextFloat();
+			if (size == 0)
+			{
+				next = 0.9f;
+			}
+
+			if (next >= 0.2f)
+			{
+				String ns = "" + rand.nextInt();
+				test.add(ns);
+				queue.add(ns);
+				size++;
+			} else
+			{
+				String toRemove = test.get(rand.nextInt(test.size()));
+				test.remove(toRemove);
+				queue.remove(toRemove);
+				size--;
+			}
+		}
+		assertTrue(test.containsAll(queue));
+		assertTrue(queue.containsAll(test));
+	}
+
+	@Test(expected = NullPointerException.class)
+	public void testNullCollection()
+	{
+		queue.removeAll(null);
+	}
+
+	@Test(expected = NullPointerException.class)
+	public void testRemoveAllNull()
+	{
+		queue.addAll(null);
+	}
+
+	@Test(expected = NullPointerException.class)
+	public void testContainsAllNull()
+	{
+		queue.containsAll(null);
+	}
+
+	@Test(expected = UnsupportedOperationException.class)
+	public void testNotSupported()
+	{
+		queue.retainAll(null);
+	}
+
+}
---
+++ src/CircularArrayQueue.java
@@ -0,0 +1,631 @@
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+import java.util.Queue;
+
+/**
+ * @author David
+ *
+ * @param <T>
+ */
+public class CircularArrayQueue<T> implements Queue<T>
+{
+	/**
+	 * The default capacity of the queue when none is provided by the user.
+	 */
+	private static final int DEFAULT_CAPACITY = 10;
+
+	/**
+	 * Array of elements added to the queue
+	 */
+	private T[] elements;
+
+	/**
+	 * Current capacity of the queue (equal to the size of the elements array)
+	 */
+	private int capacity;
+
+	/**
+	 * Location of the tail pointer
+	 */
+	private int tail = 0;
+
+	/**
+	 * Current location of the head pointer
+	 */
+	private int head = 0;
+
+	/**
+	 * Current size or number of elements in the queue
+	 */
+	private int size = 0;
+
+	/**
+	 * Used to create a queue with an initial capacity, useful if the user knows
+	 * roughly what size queue will be required ahead of time to limit the
+	 * number of resizing operations
+	 *
+	 * @param initialCapacity
+	 *            The capacity with which to create the queue
+	 */
+	@SuppressWarnings("unchecked")
+	public CircularArrayQueue(int initialCapacity)
+	{
+		elements = (T[]) new Object[initialCapacity];
+		capacity = initialCapacity;
+	}
+
+	/**
+	 * Default constructor used to create a queue when the size it may expand to
+	 * is not well known beforehand
+	 */
+	@SuppressWarnings("unchecked")
+	public CircularArrayQueue()
+	{
+		elements = (T[]) new Object[DEFAULT_CAPACITY];
+		capacity = DEFAULT_CAPACITY;
+	}
+
+	/**
+	 * Used to access the current capacity (size of element array)
+	 *
+	 * @return The current capacity of the queue
+	 */
+	public int capacity()
+	{
+		return capacity;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 *
+	 * @see java.util.Collection#size()
+	 */
+	@Override
+	public int size()
+	{
+		return size;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 *
+	 * @see java.util.Collection#contains(java.lang.Object)
+	 */
+	@Override
+	public boolean contains(Object o)
+	{
+		Iterator<T> it = iterator();
+		T elem = null;
+		if (o == null)
+		{
+			while (it.hasNext())
+			{
+				elem = it.next();
+				if (elem == null)
+				{
+					return true;
+				}
+			}
+		} else
+		{
+			while (it.hasNext())
+			{
+				elem = it.next();
+				if (o.equals(elem))
+				{
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 *
+	 * @see java.util.Collection#isEmpty()
+	 */
+	@Override
+	public boolean isEmpty()
+	{
+		return size == 0;
+	}
+
+	/**
+	 * Class used to implement the iterator for this CircularArrayQueue
+	 * collection
+	 */
+	private class CircularArrayQueueIterator implements Iterator<T>
+	{
+		/**
+		 * Initialise the start pointer to the head of the queue
+		 */
+		int start = head;
+
+		/**
+		 * Initialise the end pointer to the tail of the queue
+		 */
+		int end = tail;
+
+		/*
+		 * (non-Javadoc)
+		 * 
+		 * @see java.util.Iterator#hasNext()
+		 */
+		@Override
+		public boolean hasNext()
+		{
+			if (start == end)
+			{
+				return false;
+			} else
+			{
+				return true;
+			}
+		}
+
+		/*
+		 * (non-Javadoc)
+		 * 
+		 * @see java.util.Iterator#next()
+		 */
+		@Override
+		public T next()
+		{
+			if (!hasNext())
+			{
+				throw new NoSuchElementException("There are no more elements in this queue.");
+			}
+			T o = elements[start++];
+			if (start == capacity)
+			{
+				start = 0;
+			}
+			return o;
+		}
+
+	}
+
+	/*
+	 * (non-Javadoc)
+	 *
+	 * @see java.util.Collection#iterator()
+	 */
+	@Override
+	public Iterator<T> iterator()
+	{
+		return new CircularArrayQueueIterator();
+	}
+
+	/*
+	 * (non-Javadoc)
+	 *
+	 * @see java.util.Collection#toArray()
+	 */
+	@Override
+	public Object[] toArray()
+	{
+		return Arrays.copyOf(elements, size);
+	}
+
+	/*
+	 * (non-Javadoc)
+	 *
+	 * @see java.util.Collection#toArray(java.lang.Object[])
+	 */
+	@SuppressWarnings({ "unchecked" })
+	@Override
+	public <E> E[] toArray(E[] a)
+	{
+		E[] ret;
+		if (a.length < size)
+		{
+			ret = (E[]) new Object[size];
+		} else
+		{
+			ret = a;
+		}
+		ret = (E[]) new Object[size];
+		Iterator<T> it = iterator();
+		E elem = null;
+		int counter = 0;
+		while (it.hasNext())
+		{
+			elem = (E) it.next();
+			ret[counter++] = elem;
+		}
+		if (ret.length > size)
+		{
+			ret[size] = null;
+		}
+		return ret;
+	}
+
+	/**
+	 * Used in remove(Object o) to iterate from the point where the given
+	 * element is found, to the tail pointer, replacing each element with the
+	 * one following it
+	 *
+	 * @param i
+	 *            The index at which to start iterating from
+	 */
+	private void localRemove(int i)
+	{
+		while (i != tail)
+		{
+			if (i == capacity)
+			{
+				i = 0;
+			}
+			if (size > 1)
+			{
+				int toReplace = i;
+				elements[toReplace] = elements[toReplace + 1];
+				i++;
+			} else
+			{
+				elements[i++] = null;
+			}
+		}
+		size--;
+		tail--;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 *
+	 * @see java.util.Collection#remove(java.lang.Object)
+	 */
+	@Override
+	public boolean remove(Object o)
+	{
+		if (size == 0)
+		{
+			return false;
+		}
+		int start = head;
+		T elem;
+		if (o == null)
+		{
+			while (start != tail)
+			{
+				elem = elements[start];
+				if (elem == null)
+				{
+					localRemove(start);
+					return true;
+				} else
+				{
+					start++;
+					if (start == capacity)
+					{
+						start = 0;
+					}
+				}
+			}
+		} else
+		{
+			while (start != tail)
+			{
+				elem = elements[start];
+				if (o.equals(elem))
+				{
+					localRemove(start);
+					return true;
+				} else
+				{
+					start++;
+				}
+				if (start == capacity)
+				{
+					start = 0;
+				}
+			}
+		}
+		return false;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 *
+	 * @see java.util.Collection#containsAll(java.util.Collection)
+	 */
+	@SuppressWarnings({ "rawtypes", "unchecked" })
+	@Override
+	public boolean containsAll(Collection<?> c)
+	{
+		if (c == null)
+		{
+			throw new NullPointerException("Given collection is null.");
+		}
+		T o;
+		Iterator i = c.iterator();
+		while (i.hasNext())
+		{
+			o = (T) i.next();
+			if (!contains(o))
+			{
+				return false;
+			}
+		}
+		return true;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 *
+	 * @see java.util.Collection#addAll(java.util.Collection)
+	 */
+	@SuppressWarnings({ "rawtypes", "unchecked" })
+	@Override
+	public boolean addAll(Collection c)
+	{
+		if (c == null)
+		{
+			throw new NullPointerException("Given collection is null.");
+		}
+		Iterator<T> i = c.iterator();
+		T o = null;
+		while (i.hasNext())
+		{
+			o = i.next();
+			add(o);
+		}
+		return true;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 *
+	 * @see java.util.Collection#removeAll(java.util.Collection)
+	 */
+	@SuppressWarnings({ "unchecked" })
+	@Override
+	public boolean removeAll(Collection<?> c)
+	{
+		if (c == null)
+		{
+			throw new NullPointerException("Given collection is null");
+		}
+		Iterator<?> i = c.iterator();
+		T o = null;
+		while (i.hasNext())
+		{
+			o = (T) i.next();
+			remove(o);
+		}
+		return true;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 *
+	 * @see java.util.Collection#retainAll(java.util.Collection)
+	 */
+	@Override
+	public boolean retainAll(Collection<?> c)
+	{
+		throw new UnsupportedOperationException();
+	}
+
+	/*
+	 * (non-Javadoc)
+	 *
+	 * @see java.util.Collection#clear()
+	 */
+	@Override
+	public void clear()
+	{
+		for (int i = 0; i < elements.length; i++)
+		{
+			elements[i] = null;
+		}
+		tail = 0;
+		head = 0;
+		size = 0;
+	}
+
+	/**
+	 * Local operation used to resize the element array when the current
+	 * capacity is reached, and trying to add a new element.
+	 *
+	 * Generates a new array with twice the current capacity, and iterates from
+	 * the current head pointer, to the current tail pointer, adding all the
+	 * elements to this new object array. The current element array is then set
+	 * to this new one, and the relevant pointers are reset.
+	 *
+	 */
+	private void resize()
+	{
+		@SuppressWarnings("unchecked")
+		T[] newElements = (T[]) new Object[capacity * 2];
+		int i = head;
+		int j = tail - 1;
+		int counter = 0;
+
+		if (j == -1)
+		{
+			j = capacity - 1;
+		}
+
+		while (counter != size)
+		{
+			if (i == capacity)
+			{
+				i = 0;
+			}
+			newElements[counter++] = elements[i++];
+		}
+		tail = counter;
+		head = 0;
+		elements = newElements;
+		capacity *= 2;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 *
+	 * @see java.util.Queue#add(java.lang.Object)
+	 */
+	@Override
+	public boolean add(T e)
+	{
+		if (tail == capacity)
+		{
+			tail = 0;
+		}
+		if (tail == head && size != 0)
+		{
+			resize();
+		}
+		elements[tail++] = e;
+		size++;
+		return true;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 *
+	 * @see java.util.Queue#offer(java.lang.Object)
+	 */
+	@Override
+	public boolean offer(T e)
+	{
+		return add(e);
+	}
+
+	/*
+	 * (non-Javadoc)
+	 *
+	 * @see java.util.Queue#remove()
+	 */
+	@Override
+	public T remove()
+	{
+		if (size == 0)
+		{
+			throw new NoSuchElementException();
+		}
+		T o = elements[head++];
+		if (head == capacity)
+		{
+			head = 0;
+		}
+		size--;
+		return o;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 *
+	 * @see java.util.Queue#poll()
+	 */
+	@Override
+	public T poll()
+	{
+		if (isEmpty())
+		{
+			return null;
+		} else
+		{
+			return remove();
+		}
+	}
+
+	/*
+	 * (non-Javadoc)
+	 *
+	 * @see java.util.Queue#element()
+	 */
+	@Override
+	public T element()
+	{
+		if (size == 0)
+		{
+			throw new NoSuchElementException();
+		}
+		return elements[head];
+	}
+
+	/*
+	 * (non-Javadoc)
+	 *
+	 * @see java.util.Queue#peek()
+	 */
+	@Override
+	public T peek()
+	{
+		if (isEmpty())
+		{
+			return null;
+		} else
+		{
+			return elements[head];
+		}
+	}
+
+	/*
+	 * (non-Javadoc)
+	 *
+	 * @see java.lang.Object#hashCode()
+	 */
+	@Override
+	public int hashCode()
+	{
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + capacity;
+		result = prime * result + Arrays.hashCode(elements);
+		result = prime * result + head;
+		result = prime * result + size;
+		result = prime * result + tail;
+		return result;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 *
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
+	@Override
+	public boolean equals(Object obj)
+	{
+		if (this == obj)
+		{
+			return true;
+		}
+		if (obj == null)
+		{
+			return false;
+		}
+		if (!(obj instanceof CircularArrayQueue))
+		{
+			return false;
+		}
+		@SuppressWarnings("rawtypes")
+		CircularArrayQueue other = (CircularArrayQueue) obj;
+		if (capacity != other.capacity)
+		{
+			return false;
+		}
+		if (!Arrays.equals(elements, other.elements))
+		{
+			return false;
+		}
+		if (head != other.head)
+		{
+			return false;
+		}
+		if (size != other.size)
+		{
+			return false;
+		}
+		if (tail != other.tail)
+		{
+			return false;
+		}
+		return true;
+	}
+}
